5c22656e88f58fb886084c8624a7ca0805141c05
zraphael <raphael@wcarvalho.com>
2022-01-16 22:06:10 -0300

    contract final

--- deploy.js
+++ deploy.js
@@ -1,1 +1,24 @@
-// deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'silent border laundry alarm this return dog south impact armed jaguar ankle',
+    'https://rinkeby.infura.io/v3/e77575fe9fd0488191093518ec157df3'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account:', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    console.log('Contract deployed to', result.options.address);
+    provider.engine.stop();
+};
+
+deploy();
